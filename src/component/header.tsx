6e83c28b445cd3fbd00030c9d820e2fc9b3701f8
Johnny Gérard <john@jgerard.dev>
2025-02-24 03:51:17 +0100

    refactor: use shadow utility class

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         "flex gap-75 rounded-10 p-75",
         "border border-neutral-200 dark:border-neutral-700",
         "bg-neutral-0 dark:bg-neutral-800",
-        "[box-shadow:0_6px_10px_rgba(123,123,123,0.03)]",
+        "shadow-[0_6px_10px_rgba(123,123,123,0.03)]",
       )}
     >
       <Image
