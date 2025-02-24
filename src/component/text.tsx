0436838c236411cc05c389086c5eae2b27e892ba
Johnny Gérard <john@jgerard.dev>
2025-02-24 22:05:06 +0100

    feat: add base and h6 variants to text.tsx

--- src/component/text.tsx
+++ src/component/text.tsx
@@ -2,20 +2,25 @@ import { cn } from "@/util/cn";
 import { memo } from "react";
 
 const VARIANT = {
+  H6: "h6",
+  BASE: "base",
   SM: "sm",
 } as const;
 
 type Props = Readonly<{
   children: string;
   className?: string;
-  variant: (typeof VARIANT)[keyof typeof VARIANT];
+  variant?: (typeof VARIANT)[keyof typeof VARIANT];
 }>;
 
-const Text = ({ children, className, variant }: Props) => {
+const Text = ({ children, className, variant = VARIANT.BASE }: Props) => {
   return (
     <span
       className={cn(
         {
+          [VARIANT.H6]:
+            "text-[1.125rem]/[1.5] font-medium -tracking-[0.03125rem]",
+          [VARIANT.BASE]: "", // Use typography style inherited from root
           [VARIANT.SM]: "text-[1rem]/[1.3] -tracking-[0.0125rem]",
         }[variant],
         className,
