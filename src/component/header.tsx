11dbc88b4793af0b2e2aa46f916df1e0b20e02c8
Johnny Gérard <john@jgerard.dev>
2025-02-24 04:18:37 +0100

    feat: add navigation-menu.tsx

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,3 +1,4 @@
+import NavigationMenu from "@/component/navigation-menu";
 import NavigationMenuToggle from "@/component/navigation-menu-toggle";
 import ThemeToggle from "@/component/theme-toggle";
 import { cn } from "@/util/cn";
@@ -6,24 +7,27 @@ import { memo } from "react";
 
 const Header = () => {
   return (
-    <header
-      className={cn(
-        "flex gap-75 rounded-10 p-75",
-        "border border-neutral-200 dark:border-neutral-700",
-        "bg-neutral-0 dark:bg-neutral-800",
-        "shadow-[0_6px_10px_rgba(123,123,123,0.03)]",
-      )}
-    >
-      <Image
-        className="me-auto"
-        src="/asset/image/avatar.png"
-        alt="avatar"
-        width="40"
-        height="40"
-        priority
-      />
-      <NavigationMenuToggle />
-      <ThemeToggle />
+    <header>
+      <div
+        className={cn(
+          "flex gap-75 rounded-10 p-75",
+          "border border-neutral-200 dark:border-neutral-700",
+          "bg-neutral-0 dark:bg-neutral-800",
+          "shadow-[0_6px_10px_rgba(123,123,123,0.03)]",
+        )}
+      >
+        <Image
+          className="me-auto"
+          src="/asset/image/avatar.png"
+          alt="avatar"
+          width="40"
+          height="40"
+          priority
+        />
+        <NavigationMenuToggle />
+        <ThemeToggle />
+      </div>
+      <NavigationMenu className="mt-150" />
     </header>
   );
 };
---
+++ src/component/navigation-menu.tsx
@@ -0,0 +1,44 @@
+import AppLink from "@/component/app-link";
+import { cn } from "@/util/cn";
+import { memo } from "react";
+
+const LINKS = [
+  { href: "/", label: "Home" },
+  { href: "/blog", label: "Blog" },
+  { href: "/about", label: "About" },
+  { href: "/newsletter", label: "Newsletter" },
+] as const;
+
+type Props = {
+  className?: string;
+};
+
+const NavigationMenu = ({ className }: Props) => {
+  return (
+    <nav
+      className={cn(
+        "rounded-10 p-150",
+        "border border-neutral-200 dark:border-neutral-700",
+        "bg-neutral-0 dark:bg-neutral-800",
+        "shadow-[0px_8px_15px_rgba(8,6,25,0.06)]",
+        className,
+      )}
+    >
+      <ul className="divide-y">
+        {LINKS.map(({ href, label }) => (
+          <li
+            key={href}
+            className={cn(
+              "py-75 first:pt-0 last:pb-0",
+              "border-neutral-200 dark:border-neutral-700",
+            )}
+          >
+            <AppLink href={href}>{label}</AppLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default memo(NavigationMenu);
