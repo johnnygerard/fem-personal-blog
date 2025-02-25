3fda5a92403f7f0d3bcb6dbcbf355c3c663cfe26
Johnny Gérard <john@jgerard.dev>
2025-02-25 19:39:22 +0100

    feat: add em, ol and strong elements to mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,11 @@
 import Divider from "@/component/divider";
 import Text from "@/component/text";
+import { cn } from "@/util/cn";
 import type { MDXComponents } from "mdx/types";
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
   return {
+    em: ({ children }) => <em className="italic">{children}</em>,
     hr: () => <Divider className="first-of-type:my-400" />,
     h1: ({ children }) => (
       <h1 className="text-neutral-700 dark:text-neutral-0">
@@ -16,7 +18,18 @@ export const useMDXComponents = (components: MDXComponents): MDXComponents => {
         <Text variant="h3">{children}</Text>
       </h2>
     ),
+    ol: ({ children }) => (
+      <ol
+        className={cn(
+          "mt-150 list-outside list-decimal ps-7 marker:font-bold",
+          "flex flex-col gap-150",
+        )}
+      >
+        {children}
+      </ol>
+    ),
     p: ({ children }) => <p className="mt-150">{children}</p>,
+    strong: ({ children }) => <strong className="font-bold">{children}</strong>,
     ...components,
   };
 };
