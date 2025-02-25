4226c678f2440d34e55adae40f72784c16788f4b
Johnny Gérard <john@jgerard.dev>
2025-02-25 16:38:07 +0100

    feat: add hr, h1, h2 and p elements to mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,22 @@
+import Divider from "@/component/divider";
+import Text from "@/component/text";
 import type { MDXComponents } from "mdx/types";
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
   return {
+    hr: () => <Divider />,
+    h1: ({ children }) => (
+      <h1 className="text-neutral-700 dark:text-neutral-0">
+        <Text variant="h1">{children}</Text>
+      </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="text-neutral-700 dark:text-neutral-0">
+        {/* This is not an error: h2 is styled with h3 variant. */}
+        <Text variant="h3">{children}</Text>
+      </h2>
+    ),
+    p: ({ children }) => <p className="mt-150">{children}</p>,
     ...components,
   };
 };
