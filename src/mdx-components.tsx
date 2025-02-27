6b46c89b1da964404988c51a5c6034f18eb24c6e
Johnny Gérard <john@jgerard.dev>
2025-02-27 20:23:08 +0100

    feat: add table elements to mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,6 +6,7 @@ import type { MDXComponents } from "mdx/types";
 
 const listClassName =
   "mt-150 flex list-outside flex-col gap-150 ps-7 marker:font-bold";
+const TABLE_BORDER_COLORS = "border-neutral-200 dark:border-neutral-700";
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => ({
   blockquote: ({ children }) => (
@@ -61,6 +62,41 @@ export const useMDXComponents = (components: MDXComponents): MDXComponents => ({
     </pre>
   ),
   strong: ({ children }) => <strong className="font-bold">{children}</strong>,
+  table: ({ children }) => (
+    <table
+      className={cn(
+        "mt-150 w-full table-fixed border-collapse divide-y rounded-8",
+        // A ring is used because a border does not follow the border radius.
+        // The outline works on Chrome but not on Firefox.
+        "inset-ring inset-ring-neutral-200 dark:inset-ring-neutral-700",
+      )}
+    >
+      {children}
+    </table>
+  ),
+  tbody: ({ children }) => (
+    <tbody className={cn("divide-y", TABLE_BORDER_COLORS)}>{children}</tbody>
+  ),
+  thead: ({ children }) => (
+    <thead className={cn("divide-y", TABLE_BORDER_COLORS)}>{children}</thead>
+  ),
+  tr: ({ children }) => (
+    <tr className={cn("divide-x", TABLE_BORDER_COLORS)}>{children}</tr>
+  ),
+  td: ({ children }) => (
+    <td className={cn("px-150 pt-75 pb-100", TABLE_BORDER_COLORS)}>
+      <Text className="text-neutral-600 dark:text-neutral-400" variant="sm">
+        {children}
+      </Text>
+    </td>
+  ),
+  th: ({ children }) => (
+    <th className={cn("px-150 pt-75 pb-100 text-left", TABLE_BORDER_COLORS)}>
+      <Text className="text-neutral-700 dark:text-neutral-0" variant="h6">
+        {children}
+      </Text>
+    </th>
+  ),
   ul: ({ children }) => (
     <ul className={cn(listClassName, "list-disc")}>{children}</ul>
   ),
