e0cf9ba35c2f42adb23959fc350fa27b79a20e24
Johnny Gérard <john@jgerard.dev>
2025-02-28 04:23:59 +0100

    feat: add background variant to social-media-links.tsx

--- src/component/social-media-links.tsx
+++ src/component/social-media-links.tsx
@@ -26,23 +26,34 @@ const LINKS = [
 ] as const;
 
 type Props = {
+  className?: string;
   hasBackground?: true;
 };
 
-const SocialMediaLinks = ({ hasBackground }: Props) => {
+const SocialMediaLinks = ({ className, hasBackground }: Props) => {
   return (
-    <nav aria-label="Social media">
-      <ul className="flex gap-200">
+    <nav aria-label="Social media" className={className}>
+      <ul className={cn("flex", hasBackground ? "gap-150" : "gap-200")}>
         {LINKS.map(({ Icon, href }, index) => (
           <li key={index}>
             <AppLink
               className={cn(
-                "block h-4 rounded-2",
-                "transition-opacity data-[hovered]:opacity-70",
+                hasBackground
+                  ? [
+                      "grid h-10 w-10 place-items-center rounded-10",
+                      "bg-neutral-0 transition-colors dark:bg-neutral-800",
+                      "data-[hovered]:bg-neutral-200 dark:data-[hovered]:bg-neutral-600",
+                      "border border-neutral-200 dark:border-neutral-700",
+                      "data-[hovered]:border-neutral-300 dark:data-[hovered]:border-neutral-500",
+                    ]
+                  : [
+                      "block h-4 rounded-2",
+                      "transition-opacity data-[hovered]:opacity-70",
+                    ],
               )}
               href={href}
             >
-              <Icon className="h-full" />
+              <Icon className={cn(hasBackground ? "h-4" : "h-full")} />
             </AppLink>
           </li>
         ))}
