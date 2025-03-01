7eb35943535b2d1d45900c1b522910db70771a54
Johnny Gérard <john@jgerard.dev>
2025-03-01 22:30:21 +0100

    refactor: extract noscript.tsx component

---
+++ src/component/noscript.tsx
@@ -0,0 +1,50 @@
+import { memo } from "react";
+
+const Noscript = () => {
+  return (
+    <noscript>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: 1000,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          display: "grid",
+          placeItems: "center",
+          margin: "0 1rem",
+          pointerEvents: "none",
+        }}
+        role="alert"
+      >
+        <p
+          style={{
+            padding: "1rem",
+            backgroundColor: "#fff4f4",
+            color: "#d32f2f",
+            border: "2px solid currentColor",
+            textAlign: "center",
+            lineHeight: 1.5,
+            pointerEvents: "auto",
+            maxWidth: "30rem",
+          }}
+        >
+          JavaScript is required for this website to function properly. Please
+          ensure that it is supported and enabled in your browser settings.
+          <br />
+          To learn more, check out{" "}
+          <a
+            style={{ textDecorationLine: "underline", color: "LinkText" }}
+            href="https://enable-javascript.com/"
+          >
+            How to enable JavaScript in your browser
+          </a>
+          .
+        </p>
+      </div>
+    </noscript>
+  );
+};
+
+export default memo(Noscript);
