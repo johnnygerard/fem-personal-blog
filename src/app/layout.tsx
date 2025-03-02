441b41cbf4bc505f71f4ddc6595f5011d87edb10
Johnny Gérard <john@jgerard.dev>
2025-03-02 03:59:38 +0100

    feat: set metadataBase property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ const APP_NAME = "Personal blog";
 const DESCRIPTION = `Frontend Mentor challenge: ${APP_NAME}`;
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://example.com/placeholder"),
+  metadataBase: new URL("https://fem-personal-blog-jgerard.vercel.app"),
   title: {
     template: `%s | ${APP_NAME}`,
     default: APP_NAME,
